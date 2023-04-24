b6aa32acb41c6b12aa154754cb994a47140d1552
kentaczi <kentaczifrajedcziken@gmail.com>
2023-04-24 14:08:36 +0200

    fin

--- kola_tray.py
+++ kola_tray.py
@@ -1,8 +1,5 @@
-import os
 import numpy as np
 import cv2 as cv
-
-import coin_predict
 
 KERNEL_SHARP_FILTER = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
 
@@ -28,31 +25,3 @@ def process_and_find_coins(img_gray, img_draw):
         cv.putText(cimg, str(i_num), (i[0], i[1]), cv.FONT_HERSHEY_TRIPLEX, 2, (255, 255, 255))
 
     return cimg, radius, cords
-
-#
-# for im_name in os.listdir('./data/'):
-#     cimg = cv.imread(os.path.join('./data', im_name))
-#     img = cv.cvtColor(cimg, cv.COLOR_RGB2GRAY)
-#     img = cv.medianBlur(img, 5)
-#     img = cv.fastNlMeansDenoising(img, img, 14, 3, 5)
-#     img = cv.filter2D(img, -1, KERNEL_SHARP_FILTER)
-#     circles = cv.HoughCircles(img, cv.HOUGH_GRADIENT, 1, 50, param1=180, param2=33, minRadius=15, maxRadius=47)
-#     circles = np.uint16(np.around(circles))
-#     radius = []
-#     cords = []
-#     for i_num, i in enumerate(circles[0, :]):
-#         radius.append(i[2])
-#         cords.append((i[0], i[1]))
-#         # draw the outer circle
-#         cv.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
-#         # draw the center of the circle
-#         cv.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
-#         cv.putText(cimg, str(i_num), (i[0], i[1]), cv.FONT_HERSHEY_TRIPLEX, 2, (255, 255, 255))
-#     print(im_name)
-#     print(np.array([i for i in range(len(radius))]))
-#     print(coin_predict.predict_groups(radius))
-#     print()
-#     cimg = cv.resize(cimg, [400, 400])
-#     cv.imshow(im_name, cimg)
-# cv.waitKey(0)
-# cv.destroyAllWindows()
--- linie_tray.py
+++ linie_tray.py
@@ -1,6 +1,5 @@
 import cv2 as cv
 import numpy as np
-import os
 
 
 def process_and_find_trays(img_gray, img_draw):
@@ -22,30 +21,4 @@ def process_and_find_trays(img_gray, img_draw):
     cv.line(img, (xyposs['xmax'], xyposs['ymin']), (xyposs['xmin'], xyposs['ymin']), (0, 255, 0), 2)
     cv.line(img, (xyposs['xmax'], xyposs['ymax']), (xyposs['xmin'], xyposs['ymax']), (0, 255, 0), 2)
 
-    return img,xyposs
-
-#
-# for im_name in os.listdir('./data'):
-#     img = cv.imread(os.path.join('./data', im_name), cv.IMREAD_COLOR)
-#     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-#     gray = cv.fastNlMeansDenoising(gray, gray, 14, 3, 5)
-#
-#     edges = cv.Canny(gray, 2, 88, apertureSize=3)
-#     lines = cv.HoughLinesP(edges, 1, np.pi / 180, 90, minLineLength=100, maxLineGap=25)
-#     # print(lines.shape)
-#     xyposs = {'xmin': np.inf, 'xmax': 0, 'ymin': np.inf, 'ymax': 0}
-#     for line in lines:
-#         x1, y1, x2, y2 = line[0]
-#         xyposs['xmax'] = max(xyposs['xmax'], x1, x2)
-#         xyposs['xmin'] = min(xyposs['xmin'], x1, x2)
-#         xyposs['ymax'] = max(xyposs['ymax'], y1, y2)
-#         xyposs['ymin'] = min(xyposs['ymin'], y1, y2)
-#
-#     cv.line(img, (xyposs['xmin'], xyposs['ymin']), (xyposs['xmin'], xyposs['ymax']), (0, 255, 0), 2)
-#     cv.line(img, (xyposs['xmax'], xyposs['ymin']), (xyposs['xmax'], xyposs['ymax']), (0, 255, 0), 2)
-#     cv.line(img, (xyposs['xmax'], xyposs['ymin']), (xyposs['xmin'], xyposs['ymin']), (0, 255, 0), 2)
-#     cv.line(img, (xyposs['xmax'], xyposs['ymax']), (xyposs['xmin'], xyposs['ymax']), (0, 255, 0), 2)
-#     print(xyposs)
-#     img = cv.resize(img, [400, 400])
-#     cv.imshow(im_name, img)
-# k = cv.waitKey(0)
+    return img, xyposs
